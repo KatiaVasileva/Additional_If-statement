a9dc5f2390a0b1db9a8d6515800010b9ec115013
katiavasileva <eka.vasileva@gmail.com>
2022-11-16 00:05:17 +0500

    Task 6* is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        Задача 6
+        System.out.println("Задача 6");
+        int age = 42;
+        int salary = 110_000;
+
+        double creditLimit = 0;
+
+        if (age >= 23) {
+            creditLimit = salary * 3;
+        } else {
+            creditLimit = salary * 2;
+        }
+        if (salary >= 50_000 && salary < 80_000) {
+            creditLimit = creditLimit * 1.2;
+        } else if (salary >= 80_000) {
+            creditLimit = creditLimit * 1.5;
+        }
+        System.out.println("Мы готовы выдать вам кредитную карту с лимитом " + (int) creditLimit + " рублей.");
     }
 }
